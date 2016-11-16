f148cc37ae318a7749b1b1c22874e98f352bb7dc
Alex Plex <atellmer@gmail.com>
2016-11-17 00:40:45 +0500

    small fix for styles

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
 <article class="article" itemscope itemtype="http://schema.org/Article">
 	<iframe class="artcle__iframe" src="{{$post -> video}}" frameborder="0" allowfullscreen itemprop="video"></iframe>
 	<h1 class="artcle__title" itemprop="name">{{$post -> title}}</h1>
-	<div class="article__text" style="font-weight: 300;" itemprop="articleBody">{!!$post -> detail!!}</div>
+	<div class="article__text" itemprop="articleBody">{!!$post -> detail!!}</div>
 </article>
 
 @endsection
